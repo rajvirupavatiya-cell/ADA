8647770c851cfd2f9e423cba30942837c0cfdaf8
rajvirupavatiya-cell <rajvirupavatiya@gmail.com>
2026-05-01 19:38:29 +0200

    Create practical_4.java

---
+++ practical_4.java
@@ -0,0 +1,85 @@
+import java.util.Random;
+
+public class MergeSortAnalysis {
+
+    // Merge function
+    public static void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        // Merge the temp arrays
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Merge Sort function
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int size = 10000; // Large dataset
+        int[] data = new int[size];
+
+        Random rand = new Random();
+
+        // Generate random data
+        for (int i = 0; i < size; i++) {
+            data[i] = rand.nextInt(100000);
+        }
+
+        // Measure execution time
+        long startTime = System.nanoTime();
+
+        mergeSort(data, 0, data.length - 1);
+
+        long endTime = System.nanoTime();
+
+        long duration = endTime - startTime;
+
+        System.out.println("Execution Time: " + duration + " nanoseconds");
+        System.out.println("Execution Time: " + (duration / 1_000_000.0) + " milliseconds");
+    }
+}
