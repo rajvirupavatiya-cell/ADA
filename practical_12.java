d58edf96482ed05e54a5155f054601aae61bd019
rajvirupavatiya-cell <rajvirupavatiya@gmail.com>
2026-05-01 19:45:24 +0200

    Create practical_12.java

---
+++ practical_12.java
@@ -0,0 +1,33 @@
+public class LCS {
+
+    public static int lcs(String X, String Y) {
+        int m = X.length();
+        int n = Y.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Build DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+
+        String X = "ABCBDAB";
+        String Y = "BDCABA";
+
+        int length = lcs(X, Y);
+
+        System.out.println("Length of LCS: " + length);
+    }
+}
